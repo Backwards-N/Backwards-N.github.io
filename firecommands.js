5993b36790f1629ee5a767dc9c7b6bd4aa48b246
Peridot Lazuli <duckwhaleempire@gmail.com>
2018-03-06 15:58:15 -0700

    Update firecommands.js

--- firecommands.js
+++ firecommands.js
@@ -1,5 +1,5 @@
 function writeUserData(name, password) {
-  firebase.database().ref('users/').push.set({
+  firebase.database().ref('users/').push().set({
     username: name,
     password: password
   });

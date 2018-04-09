650ee3f4ad0ec82698ba9b1472f6247fdce6a097
Peridot Lazuli <duckwhaleempire@gmail.com>
2018-04-09 14:09:14 -0600

    Update strife.js

--- SMOD/strife.js
+++ SMOD/strife.js
@@ -1,1 +1,19 @@
+db = firebase.firestore();
 
+var choosePlayer = document.querySelector('#choosePlayer');
+var startStrife = document.querySelector('#startStrife');
+
+db.collection("players").where("owner", "==", uid)
+                .get()
+                .then(function(querySnapshot) {
+                    querySnapshot.forEach(function(doc) {
+                        // doc.data() is never undefined for query doc snapshots
+                        workingData = doc.data();
+                        console.log(workingData);
+                        choosePlayer.innerHtml += "<option value='" + doc.id "'>" + doc.name + "</option>"
+                    });
+                })
+                .catch(function(error) {
+                    console.log("Error getting documents: ", error);
+                });
+            });

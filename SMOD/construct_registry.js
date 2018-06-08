07c1a8a27655c5728607f2c285491e3b11f0afaf
Peridot Lazuli <duckwhaleempire@gmail.com>
2018-06-08 13:22:20 -0600

    Update construct_registry.js

--- SMOD/construct_registry.js
+++ SMOD/construct_registry.js
@@ -71,51 +71,51 @@ function createUnderlingAtTiers(utier, gtier, attributes) {
 
 // grist types.
 
-var amber = new gristType(1, 1, 1, 1, 1, 'one', 1, 'amber');
-var chalk = new gristType(1, 1, 1, 1, 1, 'one', 2, 'chalk');
-var shale = new gristType(1, 1, 1, 1, 1, 'one', 1, 'shale');
+var amber = new gristType(1, 1, 1, 1, 1, 'one', 1, 'Amber');
+var chalk = new gristType(1, 1, 1, 1, 1, 'one', 2, 'Chalk');
+var shale = new gristType(1, 1, 1, 1, 1, 'one', 1, 'Shale');
 
-var frosting = new gristType(1, 1, 1, 1, 1, 'one', 5, 'frosting');
-var rockCandy = new gristType(1.1, 1.2, 1.3, 1.4, 1.5, 'two', 5, 'rock candy');
+var frosting = new gristType(1, 1, 1, 1, 1, 'one', 5, 'Frosting');
+var rockCandy = new gristType(1.1, 1.2, 1.3, 1.4, 1.5, 'Two', 5, 'rock candy');
 
-var iodine = new gristType(1, 1, 1, 1, 1, 'one', 2, 'iodine');
-var crystal = new gristType(1, 1, 1, 1, 1, 'one', 1, 'crystal');
-var wood  = new gristType(1, 1, 1, 1, 1, 'one', 3, 'wood');
+var iodine = new gristType(1, 1, 1, 1, 1, 'one', 2, 'Iodine');
+var crystal = new gristType(1, 1, 1, 1, 1, 'one', 1, 'Crystal');
+var wood  = new gristType(1, 1, 1, 1, 1, 'one', 3, 'Wood');
 
 var gOne = [amber, chalk, shale, frosting, iodine, crystal, wood];
 
 // underlings.
 
-var imp = new smodConstruct(1, 1, 0, 20, 0.1, 1, 'zero', 'imp');
+var imp = new smodConstruct(1, 1, 0, 20, 0.1, 1, 'zero', 'Imp');
 var tZero = [imp];
 
-var ogre = new smodConstruct(10, 3, 0, 100, 0.04, 10, 'one', 'ogre');
-var basilisk = new smodConstruct(5, 10, 10, 70, 0.1, 7, 'one', 'basilisk');
-var lich = new smodConstruct(2, 5, 25, 30, 0.5, 1.5, 'one', 'lich');
+var ogre = new smodConstruct(10, 3, 0, 100, 0.04, 10, 'one', 'Ogre');
+var basilisk = new smodConstruct(5, 10, 10, 70, 0.1, 7, 'one', 'Basilisk');
+var lich = new smodConstruct(2, 5, 25, 30, 0.5, 1.5, 'one', 'Lich');
 var tOne = [ogre, basilisk, lich];
 
-var brute = new smodConstruct(30, 30, 15, 300, 0.1, 25, 'two', 'brute');
-var uniorc = new smodConstruct(20, 5, 0, 150, 0.5, 15, 'two', 'uniorc');
-var giclops = new smodConstruct(17, 3, 3, 500, 0.01, 15, 'two', 'giclops');
+var brute = new smodConstruct(30, 30, 15, 300, 0.1, 25, 'two', 'Brute');
+var uniorc = new smodConstruct(20, 5, 0, 150, 0.5, 15, 'two', 'Uniorc');
+var giclops = new smodConstruct(17, 3, 3, 500, 0.01, 15, 'two', 'Giclops');
 var tTwo = [brute, uniorc, giclops]
 
 // TODO: Find tier three underlings.
 
 // TODO: Find tier four underlings.
 
-var titachnid = new smodConstruct(150, 50, 15, 2500, 0.3, 45, 'five', 'titachnid');
+var titachnid = new smodConstruct(150, 50, 15, 2500, 0.3, 45, 'five', 'Titachnid');
 var tFive = [titachnid];
 
 // TODO: Find tier six underlings.
 
-var acheron = new smodConstruct(1000, 50, 0, 5750, 0.1, 25, 'seven', 'acheron');
-var drake = new smodConstruct(500, 100, 0, 2500, 0.75, 150, 'seven', 'drake');
+var acheron = new smodConstruct(1000, 50, 0, 5750, 0.1, 25, 'seven', 'Acheron');
+var drake = new smodConstruct(500, 100, 0, 2500, 0.75, 150, 'seven', 'Drake');
 var tSeven = [acheron, drake];
 
-var kraken = new smodConstruct(900, 100, 5, 6000, 0.1, 75, 'eight', 'kraken');
+var kraken = new smodConstruct(900, 100, 5, 6000, 0.1, 75, 'eight', 'Kraken');
 var tEight = [kraken];
 
-var lichQueen = new smodConstruct(250, 500, 5, 10000, 0.9, 75, 'nine', 'lich queen');
+var lichQueen = new smodConstruct(250, 500, 5, 10000, 0.9, 75, 'nine', 'Lich Queen');
 var tNine = [lichQueen];
 
 // TODO: Find tier ten underlings.
@@ -124,11 +124,11 @@ var tNine = [lichQueen];
 
 // planets.
 
-var homeworld  = new smodConstruct(0, 0, 0, 100000000000000000, 0, 600000000, 'planet', 'homeworld');
-var land = new smodConstruct(0, 0, 0, 10000000, 0, 10000, 'planet', 'land');
-var meteor = new smodConstruct(0, 0, 0, 100000, 0, 1500, 'planet', 'meteor');
-var moon = new smodConstruct(0, 0, 0, 1000000, 0, 1750, 'planet', 'moon');
-var miniMoon = new smodConstruct(0, 0, 0, 100000, 0, 1000, 'planet', 'moon');
-var skaia = new smodConstruct(0, 0, 0, 100000000, 0, 17500, 'planet', 'skaia');
+var homeworld  = new smodConstruct(0, 0, 0, 100000000000000000, 0, 600000000, 'planet', 'Homeworld');
+var land = new smodConstruct(0, 0, 0, 10000000, 0, 10000, 'planet', 'Land');
+var meteor = new smodConstruct(0, 0, 0, 100000, 0, 1500, 'planet', 'Meteor');
+var moon = new smodConstruct(0, 0, 0, 1000000, 0, 1750, 'planet', 'Dream Moon');
+var miniMoon = new smodConstruct(0, 0, 0, 100000, 0, 1000, 'planet', "Dream Moon's Moon");
+var skaia = new smodConstruct(0, 0, 0, 100000000, 0, 17500, 'planet', 'Skaia');
 
 // denizens.

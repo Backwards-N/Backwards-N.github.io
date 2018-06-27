6b09b2eef06ad278ea4be6d739a9a0f956a9da7b
Peridot Lazuli <duckwhaleempire@gmail.com>
2018-06-27 15:22:08 -0600

    Update master_registry.js

--- SMOD/master_registry.js
+++ SMOD/master_registry.js
@@ -1,6 +1,6 @@
-$.getScript("https://backwards-n.github.io/SMOD/attribute_registry.js", function() {
-   console.log("Script loaded but not necessarily executed.");
-});
+//$.getScript("https://backwards-n.github.io/SMOD/attribute_registry.js", function() {
+//   console.log("Script loaded but not necessarily executed.");
+//});
 
 $.getScript("https://backwards-n.github.io/SMOD/classpect_registry.js", function() {
    console.log("Script loaded but not necessarily executed.");

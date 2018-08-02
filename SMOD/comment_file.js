dcba2c24416bc43961f928a5d19634631d43b817
Peridot Lazuli <duckwhaleempire@gmail.com>
2018-08-02 15:23:22 -0600

    Comment file revision 1

---
+++ SMOD/comment_file.js
@@ -0,0 +1,15 @@
+/* Comment File Revision 1
+
+If you are reading this you must be a developer of some sort. This is the guide to sorting out my mess and reading my code.
+
+To begin start thinking in code structures, like the parts of a planet. The types of structures I use in my code are as follows:
+
+  Orders. These act as a set of orginizational rules you can rely on to always be the same.
+  For example, Derse always comes before Prospit.
+
+  Categorizers. These convert easily compressed memory friendly math into other code structures.
+  Essentially this allows a simple boolean or number to represent information far more information than the space it takes up.
+  This is only possible if orders are carefully followed.
+
+  Lists. Lists are arrays of similar objects that can be used interchangeably.
+  They are neccessary for categorizers to do their job.
